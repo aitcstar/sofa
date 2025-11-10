498c701778b043d9cf71bf199f0f2977979d5831
alaa mohamed aly <157356196+alaa-mohamed-aly@users.noreply.github.com>
2025-11-10 23:51:49 +0200

    update three

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.svg') }}">
 
   @if (View::hasSection('meta'))
   @yield('meta')
--- resources/views/frontend/layouts/pages.blade.php
+++ resources/views/frontend/layouts/pages.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.svg') }}">
+
 {{-- ✅ منطقة مخصصة للـ Meta Tags --}}
 @if (View::hasSection('meta'))
     @yield('meta')
